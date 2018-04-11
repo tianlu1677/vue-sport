51098483d4082b9329edc0abc475a97e166acd74
tianlu1677 <tianlu1677@gmail.com>
2018-04-11 17:35:30 +0800

    add folds

---
+++ src/base/loading/loading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>loding</div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: '正在载入...'
+      }
+    }
+  }
+</script>
+<style scoped>
+  
+</style>
