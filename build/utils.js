09170da23e515865c79df1e361e521024517bcd5
tianlu1677 <tianlu1677@gmail.com>
2018-04-11 18:00:57 +0800

    add scss load ans settings

--- build/utils.js
+++ build/utils.js
@@ -60,7 +60,12 @@ exports.cssLoaders = function (options) {
     postcss: generateLoaders(),
     less: generateLoaders('less'),
     sass: generateLoaders('sass', { indentedSyntax: true }),
-    scss: generateLoaders('sass'),
+    scss: generateLoaders('sass').concat({
+      loader: 'sass-resources-loader',
+      options: {
+        resources: path.resolve(__dirname, '../src/common/styles/_variables.scss')
+      }
+    }),
     stylus: generateLoaders('stylus'),
     styl: generateLoaders('stylus')
   }
