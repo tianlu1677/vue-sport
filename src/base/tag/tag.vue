a2a5d3648b44222adf33d09c7d6495bece419a74
tianlu1677 <tianlu1677@gmail.com>
2018-04-20 12:07:37 +0800

    add tag components

---
+++ src/base/tag/tag.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tag-wrapper">
+    <cube-button class="tag" :outline="true">
+      <span>{{tag}}</span>
+    </cube-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      tag: {
+        type: String,
+        default: 'hello'
+      }
+    }
+  }
+</script>
+<style scoped lang="scss">
+  .tag-wrapper {
+    display: inline-block;
+    margin: 5px 2.5px;
+    height: 26px;
+    border-radius: 10px;
+    .tag {
+      padding: 6px;
+      .span {
+        font-size: 14px;
+      }
+    }
+  }
+
+</style>
