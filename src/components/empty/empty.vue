33cfe7a49e465fb6b83c79a83dfd44b46c4ff615
tianlu1677 <tianlu1677@gmail.com>
2018-05-28 18:08:46 +0800

    fix loading view

--- src/components/empty/empty.vue
+++ src/components/empty/empty.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="empty">
-    <div class="content">
+    <cube-loading :size="24" class="loading" v-show="loading"></cube-loading>
+    <div class="content" v-if="!loading">
       <img src="../../common/images/logo.jpg" class="logo"/>
       <p>{{message}}</p>
     </div>
@@ -8,6 +9,7 @@
 </template>
 
 <script>
+
   export default {
     name: "empty",
     props: {
@@ -15,7 +17,18 @@
         type: String,
         default: '还没有内容哦'
       }
-    }
+    },
+    data() {
+      return {
+        loading: true
+      }
+    },
+    created() {
+      setTimeout(() => {
+        this.loading = false
+      }, 800)
+    },
+    methods: {}
   }
 </script>
 
@@ -26,6 +39,10 @@
     width: inherit;
     justify-content: center;
     text-align: center;
+    .loading {
+      position: relative;
+      text-align: center;
+    }
     .content {
       flex: 1;
       line-height: inherit;
