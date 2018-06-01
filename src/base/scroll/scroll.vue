d07912d78f8c41ccad0499819f81fb4eee5fd7e8
tianlu1677 <tianlu1677@gmail.com>
2018-06-01 14:15:48 +0800

    add scroll components

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-infinite-scroll="loadMore"
+       infinite-scroll-disabled="busy"
+       infinite-scroll-distance="5"
+       class="scroll-content">
+    <slot></slot>
+    <loading v-if="busy"></loading>
+  </div>
+</template>
+
+<script>
+  import Loading from 'base/loading/loading'
+  import Empty from 'components/empty/empty'
+
+  export default {
+    name: "scroll",
+    components: {
+      Loading,
+      Empty
+    },
+    props: {
+      busy: {
+        type: Boolean,
+        default: false
+      }
+    },
+    methods: {
+      loadMore() {
+        this.$emit('loadMore')
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
