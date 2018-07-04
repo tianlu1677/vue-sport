c82eb039eba6529e4c54b0fd5d6586067b8322d1
tianlu1677 <tianlu1677@gmail.com>
2018-07-04 15:15:56 +0800

    fix tags

--- src/pages/topics/coms/edit-tag.vue
+++ src/pages/topics/coms/edit-tag.vue
@@ -3,12 +3,27 @@
     <div class="box">
       <div class="title">填写标签</div>
       <div class="info">
-        <span class="tag" v-for="item in labelList">{{item}}</span>
-        <input type="text" placeholder="多个标签用空格分隔"
-               class="edit-tag"
-               @keyup="keyup($event)"
-               v-model="inputTag">
+        <!-- <span class="tag" v-for="item in labelList">{{item}}</span> -->
+        <!-- <input type="text" placeholder="多个标签用空格分隔" -->
+               <!-- class="edit-tag" -->
+               <!-- @keyup="keyup($event)" -->
+               <!-- v-model="inputTag"> -->
+
+                <vue-tags-input
+                  v-model="tag"
+                  :tags="tags"
+                  :add-on-key="[13, 32]"
+                  :max-tags="7"
+                  :maxlength="15"
+                  :placeholder="'多个标签用空格分隔'"
+                  :separators="[',']"
+                  style="border: none"
+
+                  @tags-changed="newTags => tags = newTags"
+                />
+        <!-- </div> -->
       </div>
+
       <div class="label-handle">
         <a href="javaScript:;" class="clean" @click="clean">取消</a>
         <a href="javaScript:;" @click.prevent="submit">保存</a>
@@ -18,8 +33,13 @@
 </template>
 
 <script>
+  import VueTagsInput from '@johmun/vue-tags-input';
+
   export default {
     name: 'edit-tag',
+    components: {
+      VueTagsInput,
+    },
     props: {
       tag_list: {
         type: Array,
@@ -30,47 +50,56 @@
       return {
         labelList: [],
         inputTag: undefined,
+        tag: '',
+        tags: []
       };
     },
+    // created() {
+    //   this.tags = this.tag_list.map((tag) => {text: tag})
+    // },
     watch: {
       tag_list() {
-        this.labelList = this.tag_list;
+        // this.labelList = this.tag_list;
+        this.tags = this.tag_list.map((tag) => ({text: tag, style: 'background-color: #FFFFFF'}))
+        console.log()
       },
     },
 
     methods: {
-      clean() {
-        this._refreshTag();
+      clean() {        
+        this.tags = this.tag_list.map((tag) => ({text: tag}))
+        this.$emit('hideEditTag');        
       },
-      submit() {
-        this.$emit('submitTag', this.labelList);
+      submit() {        
+        const new_tags = this.tags.map((tag) => tag.text)
+        this.$emit('submitTag', new_tags)
         this.$emit('hideEditTag');
       },
-      keyup(e) {
-        if (e.keyCode === 32) {
-          if (this.inputTag.trim()) {
-            if (this.labelList.length < 7) {
-              this.labelList.push(this.inputTag);
-              this.inputTag = '';
-            } else {
-              this.inputTag = '';
-            }
-          }
-        } else if (e.keyCode === 8) {
-          if (!this.inputTag) {
-            this.labelList.pop();
-          }
-        }
-      },
-      _refreshTag() {
-        this.labelList = [].concat(this.tag_list);
-        this.$emit('hideEditTag');
-      },
+      // keyup(e) {
+      //   if (e.keyCode === 32) {
+      //     if (this.inputTag.trim()) {
+      //       if (this.labelList.length < 7) {
+      //         this.labelList.push(this.inputTag);
+      //         this.inputTag = '';
+      //       } else {
+      //         this.inputTag = '';
+      //       }
+      //     }
+      //   } else if (e.keyCode === 8) {
+      //     if (!this.inputTag) {
+      //       this.labelList.pop();
+      //     }
+      //   }
+      // },
+      // _refreshTag() {
+      //   this.labelList = [].concat(this.tag_list);        
+      //   this.$emit('hideEditTag');
+      // },
     },
   };
 </script>
 
-<style scoped lang="scss">
+<style lang="scss">
   .modal-label {
     position: fixed;
     top: 0;
@@ -98,25 +127,54 @@
         margin-bottom: 36.5px;
         overflow: hidden;
         border-bottom: 1px solid #eee;
-        .edit-tag {
-          width: 100%;
-          margin-bottom: 10px;
-          padding-left: 5px;
-          font-size: 13px;
-          outline: none;
-        }
-        .tag {
-          display: inline-block;
-          margin-right: 10px;
-          margin-bottom: 10px;
-          padding-top: 6px;
-          padding-bottom: 6px;
-          padding-right: 9px;
-          padding-left: 9px;
-          border-radius: 6px;
-          border: 1px solid #333;
-          font-size: 13px;
-        }
+          
+        .vue-tags-input  {
+          .input {          
+            border: none;          
+          }
+          .tag {            
+            position: relative;
+            margin-right: 10px;
+            margin-bottom: 10px;
+            padding-top: 6px;
+            padding-bottom: 6px;
+            padding-right: 9px;
+            padding-left: 9px;
+            border-radius: 6px;
+            border: 1px solid $gray;
+            font-size: 13px;
+
+            background-color: $white;
+            color: $black;
+             
+          }
+          .tag.deletion-mark {            
+            color: $red;
+            border: 1px solid $red;
+            &:after {
+              transform: scaleX(1);
+            }
+          }                       
+        }      
+        // .edit-tag {
+        //   width: 100%;
+        //   margin-bottom: 10px;
+        //   padding-left: 5px;
+        //   font-size: 13px;
+        //   outline: none;
+        // }
+        // .tag {
+        //   display: inline-block;
+        //   margin-right: 10px;
+        //   margin-bottom: 10px;
+        //   padding-top: 6px;
+        //   padding-bottom: 6px;
+        //   padding-right: 9px;
+        //   padding-left: 9px;
+        //   border-radius: 6px;
+        //   border: 1px solid #333;
+        //   font-size: 13px;
+        // }
       }
       .label-handle {
         padding-left: 8px;
